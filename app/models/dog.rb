cd7a3decee4da147ec5ba0ae7a90710fbda4fb9c
Alice Won <alicewon4@gmail.com>
2019-12-18 15:15:03 -0800

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,12 @@
+require 'pry'
 class Dog < ApplicationRecord
+  has_many :employees
+
+
+  def self.sort_dogs
+    var = self.all.sort_by do |dog|
+      dog.employees.count
+    end
+    var.reverse
+  end
 end
